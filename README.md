63981ab348f74902e67e1fd6bf4854dc0274cb88
heyimjuani <hey@juan-i.com.ar>
2013-08-28 13:47:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Demo @ http://sandbox.juan-i.com/minRead
---
+++ minRead.js
@@ -0,0 +1,40 @@
+// minRead plugin by Juani Ruiz Echazu
+// check the demo on http://sandbox.juan-i.com/minRead
+(function($) {
+	$.fn.minRead = function( options ) {
+
+		var settings = $.extend({
+			where		: ".min-read",
+			archive		: false,
+			archiveText	: ".text"
+		}, options);
+
+		return this.each( function() {
+			var element = $(this);
+
+			var wordAmount = element.text().split(' ');
+			var timeRaw = wordAmount.length / 180;
+			var timeRound = Math.round(timeRaw);
+			if (timeRound == 0) { timeRound = 1 }
+
+			element.parent().find(settings.where).text(timeRound + " min read");
+
+			if (settings.archive) {
+				var articleLink = element.find(".article-link");
+				var articleUrl = articleLink.attr("href");
+				console.log(articleUrl);
+				$.get(articleUrl, function(data){
+					data = data.replace(/<img.+>/gi, "");
+					// console.log(data);
+					var archiveWordAmount = $(data).find(settings.archiveText).text().split(' ');
+					// console.log(archiveWordAmount);
+					var archiveTimeRaw = archiveWordAmount.length / 180;
+					console.log(archiveTimeRaw);
+					var archiveTimeRound = Math.round(archiveTimeRaw);
+					if (archiveTimeRound == 0) { archiveTimeRound = 1 }
+					element.find(settings.where).text(archiveTimeRound + " min read");
+				});
+			}
+		});
+	}
+}(jQuery));
