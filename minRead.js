eaaae7bd76410acdee1cc14002c697d890264dd0
heyimjuani <hey@juan-i.com.ar>
2013-08-29 12:28:32 -0700

    merge pull request by @flesler - made some minor mods

--- minRead.js
+++ minRead.js
@@ -1,7 +1,7 @@
 (function($) {
 	$.fn.minRead = function( options ) {
 
-		var settings = $.extend($.fn.minRead.defaults, options);
+		var settings = $.extend({}, $.fn.minRead.defaults, options);
 
 		return this.each( function() {
 			var element = $(this);
@@ -12,13 +12,13 @@
 			if (settings.archive) {
 				var articleLink = element.find(settings.anchor);
 				var articleUrl = articleLink.attr("href");
-				console.log(articleUrl);
+				// console.log(articleUrl);
 				$.get(articleUrl, function(data){
 					data = data.replace(/<img.+>/gi, "");
 					// console.log(data);
 					var text = $(data).find(settings.archiveText).text();
 					var archiveTime = calculateTime(text, settings);
-					element.find(settings.where).text(archiveTimeRound + " min read");
+					element.find(settings.where).text(archiveTime + " min read");
 				});
 			}
 		});
